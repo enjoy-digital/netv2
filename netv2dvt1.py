556d62ef5b343f615d29270c2add0e0832b95118
Florent Kermarrec <florent@enjoy-digital.fr>
2018-04-25 14:53:32 +0200

    add netv2dvt1 target to test dram, ethernet& pcie

---
+++ netv2dvt1.py
@@ -0,0 +1,371 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform
+
+from litex.soc.integration.soc_core import mem_decoder
+from litex.soc.integration.soc_sdram import *
+from litex.soc.integration.builder import *
+from litex.soc.integration.cpu_interface import get_csr_header
+from litex.soc.cores import dna, xadc
+
+from litedram.modules import MT41J128M16
+from litedram.phy import a7ddrphy
+from litedram.core import ControllerSettings
+
+from liteeth.common import convert_ip
+from liteeth.phy.rmii import LiteEthPHYRMII
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
+
+from litepcie.phy.s7pciephy import S7PCIEPHY
+from litepcie.core import LitePCIeEndpoint, LitePCIeMSI
+from litepcie.frontend.dma import LitePCIeDMA
+from litepcie.frontend.wishbone import LitePCIeWishboneBridge
+
+_io = [
+    ("clk50", 0, Pins("J19"), IOStandard("LVCMOS33")),
+
+    ("user_led", 0, Pins("M21"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("N20"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("L21"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("AA21"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("R19"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("M16"), IOStandard("LVCMOS33")),
+
+    ("serial", 0,
+        Subsignal("tx", Pins("E14")),
+        Subsignal("rx", Pins("E13")),
+        IOStandard("LVCMOS33"),
+    ),
+
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            "U6 V4 W5 V5 AA1 Y2 AB1 AB3",
+			"AB2 Y3 W6 Y1 V2 AA3"
+            ),
+            IOStandard("SSTL15")),
+        Subsignal("ba", Pins("U5 W4 V7"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("Y9"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("Y7"), IOStandard("SSTL15")),
+        Subsignal("we_n", Pins("V8"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("G1 H4 M5 L3"), IOStandard("SSTL15")),
+        Subsignal("dq", Pins(
+            "C2 F1 B1 F3 A1 D2 B2 E2 "
+            "J5 H3 K1 H2 J1 G2 H5 G3 "
+            "N2 M6 P1 N5 P2 N4 R1 P6 "
+            "K3 M2 K4 M3 J6 L5 J4 K6 "
+            ),
+            IOStandard("SSTL15"),
+            Misc("IN_TERM=UNTUNED_SPLIT_50")),
+        Subsignal("dqs_p", Pins("E1 K2 P5 M1"), IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_n", Pins("D1 J2 P4 L1"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_p", Pins("R3"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("R2"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke", Pins("Y8"), IOStandard("SSTL15")),
+        Subsignal("odt", Pins("W9"), IOStandard("SSTL15")),
+        Subsignal("reset_n", Pins("AB5"), IOStandard("LVCMOS15")),
+        Subsignal("cs_n", Pins("V9"), IOStandard("SSTL15")),
+        Misc("SLEW=FAST"),
+    ),
+
+    ("pcie_x1", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11")),
+        Subsignal("rx_n", Pins("C11")),
+        Subsignal("tx_p", Pins("D5")),
+        Subsignal("tx_n", Pins("C5"))
+    ),
+
+    ("pcie_x2", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11 B10")),
+        Subsignal("rx_n", Pins("C11 A10")),
+        Subsignal("tx_p", Pins("D5 B6")),
+        Subsignal("tx_n", Pins("C5 A6"))
+    ),
+
+    ("pcie_x4", 0,
+        Subsignal("rst_n", Pins("E18"), IOStandard("LVCMOS33")),
+        Subsignal("clk_p", Pins("F10")),
+        Subsignal("clk_n", Pins("E10")),
+        Subsignal("rx_p", Pins("D11 B10 D9 B8")),
+        Subsignal("rx_n", Pins("C11 A10 C9 A8")),
+        Subsignal("tx_p", Pins("D5 B6 D7 B4")),
+        Subsignal("tx_n", Pins("C5 A6 C7 A4"))
+    ),
+
+    ("eth_clocks", 0,
+        Subsignal("ref_clk", Pins("D17")),
+        IOStandard("LVCMOS33"),
+    ),
+
+    ("eth", 0,
+        Subsignal("rst_n", Pins("F16")),
+        Subsignal("rx_data", Pins("A20 B18")),
+        Subsignal("crs_dv", Pins("C20")),
+        Subsignal("tx_en", Pins("A19")),
+        Subsignal("tx_data", Pins("C18 C19")),
+        Subsignal("mdc", Pins("F14")),
+        Subsignal("mdio", Pins("F13")),
+        Subsignal("rx_er", Pins("B20")),
+        Subsignal("int_n", Pins("D21")),
+        IOStandard("LVCMOS33")
+     ),
+]
+
+
+class Platform(XilinxPlatform):
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xc7a35t-fgg484-2", _io, toolchain="vivado")
+        self.toolchain.bitstream_commands = [
+            "set_property CONFIG_VOLTAGE 3.3 [current_design]",
+            "set_property CFGBVS VCCO [current_design]",
+            "set_property BITSTREAM.CONFIG.CONFIGRATE 40 [current_design]",
+            "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]",
+        ]
+        self.toolchain.additional_commands = \
+            ["write_cfgmem -verbose -force -format bin -interface spix4 -size 16 "
+             "-loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.bin"]
+
+        self.add_platform_command("""
+create_clock -name pcie_phy_clk -period 10.0 [get_pins {{pcie_phy/pcie_support_i/pcie_i/inst/inst/gt_top_i/pipe_wrapper_i/pipe_lane[0].gt_wrapper_i/gtp_channel.gtpe2_channel_i/TXOUTCLK}}]
+""")
+
+
+def csr_map_update(csr_map, csr_peripherals):
+    csr_map.update(dict((n, v)
+        for v, n in enumerate(csr_peripherals, start=max(csr_map.values()) + 1)))
+
+
+def period_ns(freq):
+    return 1e9/freq
+
+
+class CRG(Module):
+    def __init__(self, platform):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
+        self.clock_domains.cd_clk200 = ClockDomain()
+        self.clock_domains.cd_clk100 = ClockDomain()
+        self.clock_domains.cd_eth = ClockDomain()
+
+        clk50 = platform.request("clk50")
+        platform.add_period_constraint(clk50, period_ns(50e6))
+
+        pll_locked = Signal()
+        pll_fb = Signal()
+        pll_sys = Signal()
+        pll_sys4x = Signal()
+        pll_sys4x_dqs = Signal()
+        pll_clk200 = Signal()
+        pll_clk50 = Signal()
+        self.specials += [
+            Instance("PLLE2_BASE",
+                     p_STARTUP_WAIT="FALSE", o_LOCKED=pll_locked,
+
+                     # VCO @ 1600 MHz
+                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=20.0,
+                     p_CLKFBOUT_MULT=32, p_DIVCLK_DIVIDE=1,
+                     i_CLKIN1=clk50, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
+
+                     # 100 MHz
+                     p_CLKOUT0_DIVIDE=16, p_CLKOUT0_PHASE=0.0,
+                     o_CLKOUT0=pll_sys,
+
+                     # 400 MHz
+                     p_CLKOUT1_DIVIDE=4, p_CLKOUT1_PHASE=0.0,
+                     o_CLKOUT1=pll_sys4x,
+
+                     # 400 MHz dqs
+                     p_CLKOUT2_DIVIDE=4, p_CLKOUT2_PHASE=90.0,
+                     o_CLKOUT2=pll_sys4x_dqs,
+
+                     # 200 MHz
+                     p_CLKOUT3_DIVIDE=8, p_CLKOUT3_PHASE=0.0,
+                     o_CLKOUT3=pll_clk200,
+
+                     # 50 MHz
+                     p_CLKOUT4_DIVIDE=32, p_CLKOUT4_PHASE=0.0,
+                     o_CLKOUT4=pll_clk50,
+            ),
+            Instance("BUFG", i_I=pll_sys, o_O=self.cd_sys.clk),
+            Instance("BUFG", i_I=pll_sys, o_O=self.cd_clk100.clk),
+            Instance("BUFG", i_I=pll_clk200, o_O=self.cd_clk200.clk),
+            Instance("BUFG", i_I=pll_sys4x, o_O=self.cd_sys4x.clk),
+            Instance("BUFG", i_I=pll_sys4x_dqs, o_O=self.cd_sys4x_dqs.clk),
+            Instance("BUFG", i_I=pll_clk50, o_O=self.cd_eth.clk),            
+            AsyncResetSynchronizer(self.cd_sys, ~pll_locked),
+            AsyncResetSynchronizer(self.cd_clk200, ~pll_locked),
+            AsyncResetSynchronizer(self.cd_clk100, ~pll_locked),
+            AsyncResetSynchronizer(self.cd_eth, ~pll_locked),
+        ]
+
+        reset_counter = Signal(4, reset=15)
+        ic_reset = Signal(reset=1)
+        self.sync.clk200 += \
+            If(reset_counter != 0,
+                reset_counter.eq(reset_counter - 1)
+            ).Else(
+                ic_reset.eq(0)
+            )
+        self.specials += Instance("IDELAYCTRL", i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
+
+
+class BaseSoC(SoCSDRAM):
+    csr_peripherals = [
+        "ddrphy",
+        "dna",
+        "xadc",
+    ]
+    csr_map_update(SoCSDRAM.csr_map, csr_peripherals)
+
+    def __init__(self, platform, **kwargs):
+        clk_freq = int(100e6)
+        SoCSDRAM.__init__(self, platform, clk_freq,
+            integrated_rom_size=0x6000,
+            integrated_sram_size=0x4000,
+            ident="NeTV2DVT1 LiteX Base SoC",
+            reserve_nmi_interrupt=False,
+            **kwargs)
+
+        self.submodules.crg = CRG(platform)
+        self.submodules.dna = dna.DNA()
+        self.submodules.xadc = xadc.XADC()
+
+        # sdram
+        self.submodules.ddrphy = a7ddrphy.A7DDRPHY(platform.request("ddram"))
+        sdram_module = MT41J128M16(self.clk_freq, "1:4")
+        self.add_constant("READ_LEVELING_BITSLIP", 3)
+        self.add_constant("READ_LEVELING_DELAY", 14)
+        self.register_sdram(self.ddrphy,
+                            sdram_module.geom_settings,
+                            sdram_module.timing_settings,
+                            controller_settings=ControllerSettings(with_bandwidth=True,
+                                                                   cmd_buffer_depth=8,
+                                                                   with_refresh=False))
+
+        # led blinking (sys
+        sys_counter = Signal(32)
+        self.sync.sys += sys_counter.eq(sys_counter + 1)
+        self.comb += platform.request("user_led", 0).eq(sys_counter[26])
+
+
+class EtherboneSoC(BaseSoC):
+    csr_peripherals = [
+        "ethphy",
+        "ethcore",
+    ]
+    csr_map_update(BaseSoC.csr_map, csr_peripherals)
+
+    def __init__(self, platform, mac_address=0x10e2d5000000, ip_address="192.168.1.50"):
+        BaseSoC.__init__(self, platform, cpu_type=None, csr_data_width=32, l2_size=64)
+
+        # ethernet mac/udp/ip stack
+        self.submodules.ethphy = LiteEthPHYRMII(self.platform.request("eth_clocks"),
+                                                self.platform.request("eth"))
+        self.submodules.ethcore = LiteEthUDPIPCore(self.ethphy,
+                                                   mac_address,
+                                                   convert_ip(ip_address),
+                                                   self.clk_freq,
+                                                   with_icmp=True)
+
+        # etherbone bridge
+        self.add_cpu_or_bridge(LiteEthEtherbone(self.ethcore.udp, 1234))
+        self.add_wb_master(self.cpu_or_bridge.wishbone.bus)
+
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.crg.cd_eth_rx.clk)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.ethphy.crg.cd_eth_tx.clk)
+
+
+class PCIeSoC(BaseSoC):
+    csr_peripherals = [
+        "pcie_phy",
+        "dma",
+        "msi",
+        "dram_dma_writer",
+        "dram_dma_reader"
+    ]
+    csr_map_update(BaseSoC.csr_map, csr_peripherals)
+
+    BaseSoC.mem_map["csr"] = 0x00000000
+    BaseSoC.mem_map["rom"] = 0x20000000
+
+    def __init__(self, platform, **kwargs):
+        BaseSoC.__init__(self, platform, csr_data_width=32, shadow_base=0x00000000, **kwargs)
+
+        # pcie phy
+        self.submodules.pcie_phy = S7PCIEPHY(platform, platform.request("pcie_x2"))
+        platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.pcie_phy.cd_pcie.clk)
+
+        # pcie endpoint
+        self.submodules.pcie_endpoint = LitePCIeEndpoint(self.pcie_phy, with_reordering=True)
+
+        # pcie wishbone bridge
+        self.submodules.pcie_wishbone = LitePCIeWishboneBridge(self.pcie_endpoint, lambda a: 1)
+        self.add_wb_master(self.pcie_wishbone.wishbone)
+
+        # pcie dma
+        self.submodules.dma = LitePCIeDMA(self.pcie_phy, self.pcie_endpoint, with_loopback=True)
+
+        # pcie msi
+        self.submodules.msi = LitePCIeMSI()
+        self.comb += self.msi.source.connect(self.pcie_phy.msi)
+        self.interrupts = {
+            "DMA_WRITER":    self.dma.writer.irq,
+            "DMA_READER":    self.dma.reader.irq
+        }
+        for i, (k, v) in enumerate(sorted(self.interrupts.items())):
+            self.comb += self.msi.irqs[i].eq(v)
+            self.add_constant(k + "_INTERRUPT", i)
+
+        # led blinking (pcie)
+        pcie_counter = Signal(32)
+        self.sync.pcie += pcie_counter.eq(pcie_counter + 1)
+        self.comb += platform.request("user_led", 1).eq(pcie_counter[26])
+
+    def generate_software_header(self):
+        csr_header = get_csr_header(self.get_csr_regions(),
+                                    self.get_constants(),
+                                    with_access_functions=False)
+        tools.write_to_file(os.path.join("software", "pcie", "kernel", "csr.h"), csr_header)
+
+
+def main():
+    platform = Platform()
+    if len(sys.argv) < 2:
+        print("missing target (base or pcie)")
+        exit()
+    if sys.argv[1] == "base":
+        soc = BaseSoC(platform)
+    elif sys.argv[1] == "etherbone":
+        soc = EtherboneSoC(platform)
+    elif sys.argv[1] == "pcie":
+        soc = PCIeSoC(platform)
+    else:
+    	ValueError
+    builder = Builder(soc, output_dir="build", csr_csv="test/csr.csv")
+    vns = builder.build()
+    soc.do_exit(vns)
+
+    if sys.argv[1] == "pcie":
+        soc.generate_software_header()
+
+if __name__ == "__main__":
+    main()
