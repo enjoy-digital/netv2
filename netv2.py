54c7e03ccb602c7af5dd02810cd3e0b600f7894e
Florent Kermarrec <florent@enjoy-digital.fr>
2018-06-19 14:02:31 +0200

    netv2: improve hdmi presentation, use split_mmcm=True to allow routing

--- netv2.py
+++ netv2.py
@@ -441,26 +441,30 @@ class NeTV2SoC(SoCSDRAM):
 
         # hdmi in 0
         if with_hdmi_in0:
-            hdmi_in0_pads = platform.request("hdmi_in")
+            hdmi_in0_pads = platform.request("hdmi_in", 0)
             self.submodules.hdmi_in0_freq = FrequencyMeter(period=self.clk_freq)
-            self.submodules.hdmi_in0 = HDMIIn(hdmi_in0_pads,
-                                             self.sdram.crossbar.get_port(mode="write"),
-                                             fifo_depth=512,
-                                             device="xc7")
+            self.submodules.hdmi_in0 = HDMIIn(
+                hdmi_in0_pads,
+                self.sdram.crossbar.get_port(mode="write"),
+                fifo_depth=512,
+                device="xc7",
+                split_mmcm=True)
             self.comb += self.hdmi_in0_freq.clk.eq(self.hdmi_in0.clocking.cd_pix.clk),
             for clk in [self.hdmi_in0.clocking.cd_pix.clk,
                         self.hdmi_in0.clocking.cd_pix1p25x.clk,
                         self.hdmi_in0.clocking.cd_pix5x.clk]:
                 self.platform.add_false_path_constraints(self.crg.cd_sys.clk, clk)
+            self.platform.add_period_constraint(platform.lookup_request("hdmi_in", 0).clk_p, period_ns(148.5e6))
 
         # hdmi out 0
         if with_hdmi_out0:
             hdmi_out0_dram_port = self.sdram.crossbar.get_port(mode="read", dw=16, cd="hdmi_out0_pix", reverse=True)
-            self.submodules.hdmi_out0 = VideoOut(platform.device,
-                                                 platform.request("hdmi_out"),
-                                                 hdmi_out0_dram_port,
-                                                 "ycbcr422",
-                                                 fifo_depth=4096)
+            self.submodules.hdmi_out0 = VideoOut(
+                platform.device,
+                platform.request("hdmi_out", 0),
+                hdmi_out0_dram_port,
+                "ycbcr422",
+                fifo_depth=4096)
             for clk in [self.hdmi_out0.driver.clocking.cd_pix.clk,
                         self.hdmi_out0.driver.clocking.cd_pix5x.clk]:
                 self.platform.add_false_path_constraints(self.crg.cd_sys.clk, clk)
